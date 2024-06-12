ac1bb451cdb811ff2ab0a4bbff13004bd44509ee
antONget <64537429+antONget@users.noreply.github.com>
2024-06-12 20:14:04 +0300

    Add files via upload

---
+++ database/requests.py
@@ -0,0 +1,182 @@
+from database.models import User
+from database.models import async_session
+
+# Для тестов функций надо раскоментить, а выше закоменть
+# from models import User
+# from models import async_session
+
+
+
+
+import logging as lg
+
+
+from sqlalchemy import select #, update, delete
+
+
+
+""" ------------- ADD METHODS -------------"""
+
+
+
+
+async def add_user(data: dict):
+    """
+    ```
+    add user to database:
+        data : dict
+        {
+            'id': int, 
+            'username': str (without @), 
+            'referral_link': str, 
+            'referral_users': str ('123,1234,12345'),
+            'ton_balance': int (default=0)
+        }
+    ```
+    """
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.id == data["id"]))
+
+        if not user:
+            session.add(User(**data))
+            await session.commit()
+
+
+async def add_referral_user(main_user_id: int, referral_user_id: int):
+    """
+    ```
+    add referral_user to "referral_users":
+        main_user_id : int   (id главного пользователя)
+        referral_user_id: int   (id реферала)
+    ```
+    """
+    async with async_session() as session:
+        user: User = await session.scalar(select(User).where(User.id == main_user_id))
+        if user:
+            if len(user.referral_users) != 0:
+                user.referral_users += f",{referral_user_id}"
+                await session.commit()
+            else:
+                user.referral_users += f"{referral_user_id}"
+                await session.commit()
+
+
+
+async def increase_ton_balance(tg_id: int, s: float):
+    """
+    ```
+    increase ton_balance:
+        tg_id : int
+        s : float  (sum of toncoins)
+    ```
+    """
+    async with async_session() as session:
+        user: User = await session.scalar(select(User).where(User.id == tg_id))
+        if user:
+            
+            user.ton_balance = format(user.ton_balance + s, '.4f')
+            
+                
+            await session.commit()
+
+
+
+
+
+
+
+
+""" ------------- GET METHODS -------------"""
+async def get_balance(tg_id: int):
+    """
+    ```
+    returns:
+        ton_balance
+    ```
+    """
+    async with async_session() as session:
+        user: User = await session.scalar(select(User).where(User.id == tg_id))
+        if user:
+            return user.ton_balance
+        else:
+            return 0
+
+
+async def get_referral_link(tg_id: int):
+    """
+    ```
+    returns:
+        referral_link : str
+    ```
+    """
+    async with async_session() as session:
+        user: User = await session.scalar(select(User).where(User.id == tg_id))
+        if user:
+            return user.referral_link
+        else:
+            return None
+        
+
+
+async def _get_username_from_id(tg_id: int):
+    '''```code
+    returns:
+        username for function "get_referral_users" 
+    ```'''
+    async with async_session() as session:
+        user: User = await session.scalar(select(User).where(User.id == tg_id))
+        if user:
+            return user.username
+        else:
+            return None
+
+async def get_referral_users(tg_id: int):
+    '''```python
+    returns:
+        По вашей реферальной ссылке подписались на бот {len(users)} пользователей:\n\n
+        
+        1. @username1
+        2. @username2
+        3. @username3
+    ```'''
+    async with async_session() as session:
+        users = (await session.scalar(select(User).where(User.id == tg_id))).referral_users
+
+        if users:
+            users = users.split(",")
+            s = f'По вашей реферальной ссылке подписались на бот {len(users)} пользователей:\n\n'
+            c = 1
+            for user_id in users:
+                s += f"{c}. @{await _get_username_from_id(int(user_id))}\n"
+                c += 1
+
+
+            return s
+        else:
+            return None
+
+
+
+async def get_all_users() -> list[User]:
+    async with async_session() as session:
+        users = await session.scalars(select(User))
+        return users
+
+
+
+
+
+from asyncio import run, sleep
+
+# print([ i.username for i in run(get_all_users())])
+
+# run(add_user({"id":6663237590, "username":"paul", "referral_link":'https://t.me/PBAsync', 'referral_users':'123,1234,12345'}))
+# print(run(get_balance(6663237590)))
+# print(run(get_referral_link(1234)))
+# print(run(get_referral_users(6663237590)))
+# print(run(get_referral_users(1234)))
+# run(add_referral_user(6663237590, 6663237590))
+# run(increase_ton_balance(6663237590, 0.0005))
+
+
+# print(format(0.0005, '.4f'))

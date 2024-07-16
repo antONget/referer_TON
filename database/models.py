583652e440ceda20baffe75185292ed2e9e19f8e
antONget <a.l.ponomare@mail.ru>
2024-07-16 16:36:36 +0300

    вариант для теста 18

--- database/models.py
+++ database/models.py
@@ -23,8 +23,8 @@ class User(Base):
     referral_users: Mapped[str] = mapped_column(String, default="")  # id_1,id_2,...
     referer_id: Mapped[int] = mapped_column(Integer, default=0)
     ton_balance: Mapped[int] = mapped_column(Float, default=0.0)
-    status: Mapped[str] = mapped_column(String(200), default="None")
-    crypto_ton_addr: Mapped[str] = mapped_column(String, default="None")
+    status: Mapped[str] = mapped_column(String(200), default="create")
+    crypto_ton_addr: Mapped[str] = mapped_column(String, default="pass")
 
 
 async def async_main():
